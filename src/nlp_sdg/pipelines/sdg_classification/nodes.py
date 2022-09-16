f9f903e9cc053cf6c1fbfea1255d5e89b86682e1
Nthiki <nthikeng@explore-ai.net>
2022-09-16 07:56:19 +0200

    I've made a change

--- src/nlp_sdg/pipelines/sdg_classification/nodes.py
+++ src/nlp_sdg/pipelines/sdg_classification/nodes.py
@@ -3,5 +3,6 @@ This is a boilerplate pipeline 'sdg_classification'
 generated using Kedro 0.18.2
 """
 def dummy_node(data):
+    #dummy node
     print("Text Classification dummy node completed")
     return 5
