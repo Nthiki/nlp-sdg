023b2a7f76de0bbd14b7a9b34895eb1ea1f1a564
Luka Beverin <luka@explore-datascience.net>
2022-09-22 14:41:54 +0000

    Twitter cleaning data and sentiment analysis

--- src/nlp_sdg/pipelines/data_engineering/nodes.py
+++ src/nlp_sdg/pipelines/data_engineering/nodes.py
@@ -7,6 +7,20 @@ import numpy as np
 import pandas as pd
 from pyspark.sql import DataFrame
 
+#twitter cleaning
+import re
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+
+''' ================================== 
+ Dummy code
+ ==================================== '''
+
 
 def dummy_node(data: DataFrame) -> DataFrame:
     """Dummy node to read data
@@ -19,3 +33,76 @@ def dummy_node(data: DataFrame) -> DataFrame:
 
 
     return data
+
+
+''' ================================== 
+ Data engineering functions for Team A
+ ==================================== '''
+
+
+
+
+
+''' ================================== 
+ Data engineering functions for Team B
+ ==================================== '''
+
+
+def _clean_tweet(tweet):
+    '''
+    tweet: String
+           Input Data
+    tweet: String
+           Output Data
+           
+    func: Removes hashtag symbol in front of a word
+          Replace URLs with a space in the message
+          Replace ticker symbols with space. The ticker symbols are any stock symbol that starts with $.
+          Replace  usernames with space. The usernames are any word that starts with @.
+          Replace everything not a letter or apostrophe with space
+          Remove single letter words
+          filter all the non-alphabetic words, then join them again
+
+    '''
+    
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+    tweet = re.sub('\$[a-zA-Z0-9]*', ' ', tweet)
+    tweet = re.sub('\@[a-zA-Z0-9]*', ' ', tweet)
+    tweet = re.sub('[^a-zA-Z\']', ' ', tweet)
+    tweet = re.sub(r'\s+', " ", tweet)
+    tweet = ' '.join( [w for w in tweet.split() if len(w)>1] )
+    
+    return tweet
+
+
+def _lemmatize(text: str) -> str:
+    stop_words = stopwords.words('english')
+    lemmatizer = WordNetLemmatizer()
+    text = text.lower()
+    text = [lemmatizer.lemmatize(word) for word in text.split() if word not in stop_words]
+    return ' '.join(text)
+
+
+
+def data_preprocessing(data: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Function takes in the whole dataframe and carries out the following preprocessing steps:
+    
+    1. Removes strings etc
+    2. Lemmatization and removing stopwords
+    
+    Then return the dataframe with an added column that has the cleaned version of the text
+    '''
+    data['clean_text'] = data['Text'].apply(lambda x:_clean_tweet(x))
+    data['clean_text'] = data['clean_text'].apply(_lemmatize)
+    #print(data['clean_text'][0])
+    
+    return data
+
+
+
+
+
+''' ================================== 
+ Data engineering functions for Team C
+ ==================================== '''
--- src/nlp_sdg/pipelines/data_engineering/pipeline.py
+++ src/nlp_sdg/pipelines/data_engineering/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.2
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from nlp_sdg.pipelines.data_engineering.nodes import dummy_node
+from nlp_sdg.pipelines.data_engineering.nodes import dummy_node, data_preprocessing
 
 
 
@@ -19,12 +19,19 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="model_input_data",
                 name="dummy_node",
             ),
+              node(
+                func=data_preprocessing,
+                inputs="twitter_data",
+                outputs="cleaned_twitter_data",
+                name="create_twitter_input_table_node",
+            ),
+
         ]
     )
     data_engineering = pipeline(
         pipe=pipeline_instance,
-        inputs="sdg_data",
+        inputs=["sdg_data","twitter_data"],
         namespace = "data_engineering",
-        outputs = "model_input_data"
+        outputs = ["model_input_data", "cleaned_twitter_data"]
     )
     return data_engineering
