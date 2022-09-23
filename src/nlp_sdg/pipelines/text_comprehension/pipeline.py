47938853f1fb1278c6fc7be5e5d66e094c3c2108
kojosbk <97066032+kojosbk@users.noreply.github.com>
2022-09-23 15:19:39 +0000

    location and organization functions added

--- src/nlp_sdg/pipelines/text_comprehension/pipeline.py
+++ src/nlp_sdg/pipelines/text_comprehension/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.2
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from nlp_sdg.pipelines.text_comprehension.nodes import dummy_node
+from nlp_sdg.pipelines.text_comprehension.nodes import dummy_node,locationOrganization
 
 
 
@@ -19,11 +19,18 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="comprehension_output",
                 name="dummy_node",
             ),
+            node(
+                func=locationOrganization,
+                inputs="sdg_text_data",
+                outputs="locations_and_org_data",
+                name="locationOrganization_node",
+            ),            
         ]
     )
     text_comprehension = pipeline(
         pipe=pipeline_instance,
-        inputs="model_input_data",
-        namespace = "text_comprehension"
+        inputs=["model_input_data","sdg_text_data"],
+        namespace = "text_comprehension",
+        outputs="locations_and_org_data"
     )
     return text_comprehension
