1dae0b4ce8d1357a35c1c0c0b3b58aa69b7096ba
kojosbk <97066032+kojosbk@users.noreply.github.com>
2022-09-23 01:12:16 +0000

    added a quention and answer function to the text summarizer section

--- src/nlp_sdg/pipelines/text_comprehension/nodes.py
+++ src/nlp_sdg/pipelines/text_comprehension/nodes.py
@@ -2,6 +2,22 @@
 This is a boilerplate pipeline 'text_comprehension'
 generated using Kedro 0.18.2
 """
+import numpy as np
+import pandas as pd
+from allennlp.predictors.predictor import Predictor
+predictor = Predictor.from_path('https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2020.03.19.tar.gz')
+
 def dummy_node(data):
     print("Text Comprehension dummy node completed")
     return 5
+
+def qes_and_ans(data,questions = "Is Shell implicated?"):
+
+        def qestions(data,question):
+            result = []
+            for i in range(len(data["text"])):
+                result.append(predictor.predict(passage=data["text"][i], question=question)["best_span_str"])
+            return result
+
+        data[questions] = qestions(data, question = questions)
+        return data[questions]
--- src/nlp_sdg/pipelines/text_comprehension/pipeline.py
+++ src/nlp_sdg/pipelines/text_comprehension/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.2
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from nlp_sdg.pipelines.text_comprehension.nodes import dummy_node
+from nlp_sdg.pipelines.text_comprehension.nodes import dummy_node,qes_and_ans
 
 
 
@@ -19,11 +19,18 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="comprehension_output",
                 name="dummy_node",
             ),
+            node(
+                func=qes_and_ans,
+                inputs="sdg_text_data",
+                outputs="qes_and_ans_data",
+                name="qes_and_ans_node",
+            ),
         ]
     )
     text_comprehension = pipeline(
         pipe=pipeline_instance,
-        inputs="model_input_data",
-        namespace = "text_comprehension"
+        inputs=["model_input_data","sdg_text_data"],
+        namespace = "text_comprehension",
+        outputs="qes_and_ans_data"
     )
     return text_comprehension
