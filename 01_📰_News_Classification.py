53fee1a1121cbadf5e809edb415210c21bae01a4
Luka Beverin <luka@explore-datascience.net>
2022-09-26 11:05:39 +0000

    Added pages in streamlit

--- 01_📰_News_Classification.py
+++ 01_📰_News_Classification.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as px
 import numpy as np 
+import pandas as pd
 
 from kedro.io import DataCatalog
 import yaml
@@ -18,11 +19,15 @@ io = DataCatalog(
     }
 )
 
+
+#st.markdown("# UN SDG Internship Project🎈")
+st.sidebar.markdown("# This feature classifies new articles")
+
 #Title
 
 st.title('UN SDG Internship Project')
-
-@st.cache
+@st.experimental_memo
+#@st.cache
 def load_data(data_name):
     data = io.load(data_name)
     #can add extra stuff here
@@ -31,15 +36,31 @@ def load_data(data_name):
 
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
+# Load rows of data into the dataframe.
 #data_name = "sdg_text_data"
 data = load_data("sdg_text_data")
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("")
 
+
+df = data.head(5)
+
+st.subheader('Select news article')
+option = st.selectbox(
+    'Select a news article',
+    ('Shell goes under', 'Coastal woes in Durban', 'CEO of Shell steps down'))
+
+st.write('You selected:', option)
+
+#When we select an article, is it possible to show the article?
+
+st.dataframe(df)
+
 # Histogram of SDG label distribution
 
-st.subheader('SDG label distribution')
+
+st.subheader('Training data set')
+st.markdown('##### SDG label distribution')
 
 hist_values = np.histogram(data['sdg'], bins=15, range=(0,16))[0]
 
---
+++ pages/03_👥_Text_comprehension.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("# Text comprehension")
+st.sidebar.markdown("# Text comprehension")
