f867af6726f472dfcd24cb503e066b4cd8671868
lawalsegun2025 <lawalsegun2025@gmail.com>
2022-09-26 11:48:39 +0000

    Function that takes in a data frame and returns a data frame that creates summarized column of the text column

--- src/nlp_sdg/pipelines/text_comprehension/pipeline.py
+++ src/nlp_sdg/pipelines/text_comprehension/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.2
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from nlp_sdg.pipelines.text_comprehension.nodes import dummy_node
+from nlp_sdg.pipelines.text_comprehension.nodes import dummy_node, summarize_text
 
 
 
@@ -19,11 +19,18 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="comprehension_output",
                 name="dummy_node",
             ),
+            node(
+                func=summarize_text,
+                inputs="sdg_text_data",
+                outputs="summarize_text_data",
+                name="summarize_text_node",
+            )
         ]
     )
     text_comprehension = pipeline(
         pipe=pipeline_instance,
-        inputs="model_input_data",
-        namespace = "text_comprehension"
+        inputs=["model_input_data", "sdg_text_data"],
+        namespace = "text_comprehension",
+        outputs= "summarize_text_data",
     )
     return text_comprehension
