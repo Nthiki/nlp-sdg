592f19c35fe156c2481df50493e71b00a52b2241
DanielOpanubi <opanubidaniel@gmail.com>
2022-09-26 18:54:57 +0100

    Added CSV conversion node

--- src/nlp_sdg/pipelines/data_engineering/nodes.py
+++ src/nlp_sdg/pipelines/data_engineering/nodes.py
@@ -6,6 +6,7 @@ from typing import Dict
 import numpy as np
 import pandas as pd
 from pyspark.sql import DataFrame
+import sqlite3
 
 
 def dummy_node(data: DataFrame) -> DataFrame:
@@ -18,3 +19,28 @@ def dummy_node(data: DataFrame) -> DataFrame:
     """
 
     return data
+
+
+def convert_to_csv(data: DataFrame) -> DataFrame:
+    connection = sqlite3.connect(data)
+    cursor = connection.cursor()
+    
+    # Execute the query
+    cursor.execute('select * from mydata')
+    # Get Header Names (without tuples)
+    colnames = [desc[0] for desc in cursor.description]
+    # Get data in batches
+    while True:
+        # Read the data
+        df = pd.DataFrame(cursor.fetchall())
+        # We are done if there are no data
+        if len(df) == 0:
+            break
+        # Let us write to the file
+        else:
+            df.to_csv(f, header=colnames)
+
+    cursor.close()
+    connection.close()
+    
+    return df
