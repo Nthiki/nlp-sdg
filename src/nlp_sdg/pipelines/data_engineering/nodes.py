8a660a9f8f734c7a2a2a016359d71320fb68f992
lista <listaabutto@gmail.com>
2022-09-27 07:24:01 +0000

    Function to clean SDG text (URLs, hashtags, punctuations etc)

--- src/nlp_sdg/pipelines/data_engineering/nodes.py
+++ src/nlp_sdg/pipelines/data_engineering/nodes.py
@@ -6,6 +6,7 @@ from typing import Dict
 import numpy as np
 import pandas as pd
 from pyspark.sql import DataFrame
+from nltk.stem import WordNetLemmatizer
 
 
 def dummy_node(data: DataFrame) -> DataFrame:
@@ -36,5 +37,38 @@ def clean_agreement(data:pd.DataFrame) -> DataFrame:
 
     data = data.loc[(data['labels_negative'] < data['labels_positive']) & (data['agreement'] >= 0.4)]
     print("success")
+
+    return data
+
+def clean_text(data:pd.DataFrame) -> DataFrame:
+    
+    """Converts apostrophe suffixes to words, replace webpage links with url, 
+    annotate hashtags and mentions, remove a selection of punctuation, and convert all words to lower case.
+    Args:
+        df (DataFrame): dataframe containing 'text' column to convert
+    Returns:
+        df (DataFrame): dataframe with converted 'text' column 
+    """
+    def word_lemma(words):
+        lemmatizer = WordNetLemmatizer()
+        lemma = [lemmatizer.lemmatize(word) for word in words]
+        return ''.join([l for l in lemma])
+    import string
+    def remove_extras(post):
+        punc_numbers = string.punctuation + '0123456789'
+        return ''.join([l for l in post if l not in punc_numbers])
+    
+    def sdglabler(df: pd.DataFrame):
+        sdgLables = {1: "No poverty", 2: "Zero Hunger", 3: "Good Health and well-being", 4: "Quality Education", 5: "Gender equality", 6: "Clean water and sanitation", 7: "Affordable and clean energy", 9: "Industry, Innovation and Infrustructure", 8: "Decent work and economic growth",
+                    10: "Reduced Inequality", 13: "Climate Action", 11: "Sustainable cites and communities", 12: "Responsible consumption and production", 14: "life below water", 15: "Life on land", 16: "Peace , Justice and strong institutions", 17: "Partnership for the goals"}
+        df['SDG_Labels'] = df['sdg'].map(sdgLables)
+
+    #Lower case
+    data["text"] = data["text"].str.lower()
+    #Removal of Punctuation
+    data["text"] = data['text'].apply(remove_extras)
+    data["text"] = data['text'].apply(word_lemma)    
+    # Map the target variable name to their code for better understanding
+    sdglabler(data)
 
     return data
--- src/nlp_sdg/pipelines/data_engineering/pipeline.py
+++ src/nlp_sdg/pipelines/data_engineering/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.2
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from nlp_sdg.pipelines.data_engineering.nodes import dummy_node, clean_agreement
+from nlp_sdg.pipelines.data_engineering.nodes import dummy_node, clean_agreement, clean_text
 
 
 
@@ -25,13 +25,19 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="cleaned_agreement_data",
                 name="clean_agreement_node"
             ),
+            node(
+                func=clean_text,
+                inputs="cleaned_agreement_data", 
+                outputs="cleaned_text_data",
+                name="clean_text_node"
+            ),
         ]
     )
     data_engineering = pipeline(
         pipe=pipeline_instance,
         inputs=["sdg_data", "sdg_text_data"],
         namespace = "data_engineering",
-        outputs = ["model_input_data", "cleaned_agreement_data"]
+        outputs = ["model_input_data", "cleaned_agreement_data", "cleaned_text_data"]
     )
     return data_engineering
     
