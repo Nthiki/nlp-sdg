4a5b3fe45a1e48010bc41de244189d32f4f93849
kojosbk <97066032+kojosbk@users.noreply.github.com>
2022-09-27 08:51:49 +0000

    Update nodes.py

--- src/nlp_sdg/pipelines/text_comprehension/nodes.py
+++ src/nlp_sdg/pipelines/text_comprehension/nodes.py
@@ -4,6 +4,7 @@ generated using Kedro 0.18.2
 """
 import numpy as np
 import pandas as pd
+from pyspark.sql import DataFrame
 from allennlp.predictors.predictor import Predictor
 predictor = Predictor.from_path('https://storage.googleapis.com/allennlp-public-models/bidaf-elmo-model-2020.03.19.tar.gz')
 
@@ -11,8 +12,15 @@ def dummy_node(data):
     print("Text Comprehension dummy node completed")
     return 5
 
-def qes_and_ans(data,questions = "Is Shell implicated?"):
+def qes_and_ans(data,questions = "which resorces are mentioned?"):
+        """question and answer node meant to produce answers to artiles in a dataframe
 
+        Args:
+            data: Data containing a text column.
+        Returns:
+            data: a dataframe answering the asked question based on the articles in each row 
+        """
+        data = data.head(2)
         def qestions(data,question):
             result = []
             for i in range(len(data["text"])):
