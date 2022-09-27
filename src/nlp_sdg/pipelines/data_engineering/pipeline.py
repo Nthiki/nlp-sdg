b847feb173639727a557694c59ec1545343dd59a
MaureenMbugua <maureennyathirambugua@gmail.com>
2022-09-27 11:32:20 +0300

    Added twitter data cleaning function.
    I have moved the data cleaning function from the twitter analytics pipeline to the data engineering pipeline.

--- src/nlp_sdg/pipelines/data_engineering/pipeline.py
+++ src/nlp_sdg/pipelines/data_engineering/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.2
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from nlp_sdg.pipelines.data_engineering.nodes import dummy_node
+from nlp_sdg.pipelines.data_engineering.nodes import dummy_node, data_preprocessing
 
 
 
@@ -19,12 +19,18 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="model_input_data",
                 name="dummy_node",
             ),
+            node(
+                func= data_preprocessing,
+                inputs='tweet_text_data',
+                outputs='clean_tweet_data',
+                name='data_preprocessing_node'
+            )
         ]
     )
     data_engineering = pipeline(
         pipe=pipeline_instance,
-        inputs="sdg_data",
+        inputs=["sdg_data", "tweet_text_data"],
         namespace = "data_engineering",
-        outputs = "model_input_data"
+        outputs = ["model_input_data","clean_tweet_data"]
     )
     return data_engineering
--- src/nlp_sdg/pipelines/twitter_analytics/pipeline.py
+++ src/nlp_sdg/pipelines/twitter_analytics/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.2
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from nlp_sdg.pipelines.twitter_analytics.nodes import dummy_node, data_preprocessing
+from nlp_sdg.pipelines.twitter_analytics.nodes import dummy_node
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -18,18 +18,13 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="analytics_output",
                 name="dummy_node",
             ),
-            node(
-                func= data_preprocessing,
-                inputs='tweet_text_data',
-                outputs='clean_tweet_data',
-                name='data_preprocessing_node'
-            )
+        
         ]
     )
     twitter_analytics = pipeline(
         pipe=pipeline_instance,
-        inputs=["model_input_data", "tweet_text_data"],
-        outputs=["clean_tweet_data", "analytics_output"],
+        inputs="model_input_data", 
+        outputs="analytics_output",
         namespace = "twitter_analytics"
     )
     return twitter_analytics
