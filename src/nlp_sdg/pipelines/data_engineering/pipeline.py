198a2cc4b946b850e9a748599b284599a515ae78
Adegbem <adegbem@gmail.com>
2022-09-27 12:28:08 +0000

    append nodes needed for data pre-processing

--- src/nlp_sdg/pipelines/data_engineering/pipeline.py
+++ src/nlp_sdg/pipelines/data_engineering/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.2
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from nlp_sdg.pipelines.data_engineering.nodes import dummy_node
+from nlp_sdg.pipelines.data_engineering.nodes import clean_agreement, preprocess_data, data_balancing
 
 
 
@@ -14,10 +14,22 @@ def create_pipeline(**kwargs) -> Pipeline:
         
         [
             node(
-                func=dummy_node,
+                func=clean_agreement,
                 inputs="sdg_data",
+                outputs="filtered_data",
+                name="clean_agreement_node",
+            ),
+            node(
+                func=preprocess_data,
+                inputs="filtered_data",
+                outputs="cleaned_data",
+                name="preprocess_data_node",
+            ),
+            node(
+                func=data_balancing,
+                inputs="cleaned_data",
                 outputs="model_input_data",
-                name="dummy_node",
+                name="data_balancing_node",
             ),
         ]
     )
